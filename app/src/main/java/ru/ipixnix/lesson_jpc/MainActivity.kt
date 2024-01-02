8958fc468b3f0fdd4c77fc6395521137f471d644
iPixNix <ipixnix@ya.ru>
2024-01-03 01:21:21 +0300

    Added Column
    Adding a Column and moving Text1, Text2, Text3 elements to this Column.

--- app/src/main/java/ru/ipixnix/lesson_jpc/MainActivity.kt
+++ app/src/main/java/ru/ipixnix/lesson_jpc/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.ipixnix.lesson_jpc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,18 +25,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ScreenMain() {
 
-    // Элемент Text 1
-    Text(
-        text = "Text 1" // Текст содержит слово "Text 1"
-    )
+    // Колонка
+    Column {
+        // Элемент Text 1
+        Text(
+            text = "Text 1" // Текст содержит слово "Text 1"
+        )
 
-    // Элемент Text 2
-    Text(
-        text = "Text 2" // Текст содержит слово "Text 2"
-    )
+        // Элемент Text 2
+        Text(
+            text = "Text 2" // Текст содержит слово "Text 2"
+        )
 
-    // Элемент Text 3
-    Text(
-        text = "Text 3" // Текст содержит слово "Text 3"
-    )
+        // Элемент Text 3
+        Text(
+            text = "Text 3" // Текст содержит слово "Text 3"
+        )
+    }
 }
