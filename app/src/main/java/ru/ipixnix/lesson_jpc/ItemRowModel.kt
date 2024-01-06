cef61b9ef3c09045844680122923f91df6fd999d
iPixNix <ipixnix@ya.ru>
2024-01-06 18:27:34 +0300

    JPC-20 BottomNavigation
    Сброс MainActivity для урока 20 BottomNavigation.

--- app/src/main/java/ru/ipixnix/lesson_jpc/ItemRowModel.kt
+++
@@ -1,8 +0,0 @@
-package ru.ipixnix.lesson_jpc
-
-/** Пользовательский тип элемента Горизонтального списка */
-data class ItemRowModel(
-    val imageId: Int, // Идендификатор изображения
-    val title: String, // Заголовок элемента
-    val content: String // Контент элемента
-)
