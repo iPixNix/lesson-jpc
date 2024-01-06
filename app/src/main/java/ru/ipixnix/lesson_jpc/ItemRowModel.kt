39708b2abf7fe2f7e28947aab55d29ae905c83a1
iPixNix <ipixnix@ya.ru>
2024-01-06 04:55:51 +0300

    Update: Раскрывающийся список
    Обновление списка с урока 07.
    
    Update: Список переделан в вертикальный.
    
    New: Добавлена возможность развёртывать и свёртывать многострочный контент элементов списка.

--- app/src/main/java/ru/ipixnix/lesson_jpc/ItemRowModel.kt
+++ app/src/main/java/ru/ipixnix/lesson_jpc/ItemRowModel.kt
@@ -3,5 +3,6 @@ package ru.ipixnix.lesson_jpc
 /** Пользовательский тип элемента Горизонтального списка */
 data class ItemRowModel(
     val imageId: Int, // Идендификатор изображения
-    val title: String // Заголовок элемента
+    val title: String, // Заголовок элемента
+    val content: String // Контент элемента
 )
