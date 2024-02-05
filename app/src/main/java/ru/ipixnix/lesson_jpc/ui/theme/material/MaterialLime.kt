237843a4610557188ebc6b5d75ea20e13453c18d
iPixNix <ipixnix@ya.ru>
2024-02-05 12:15:12 +0300

    New: Material Lime
    New: Добавлены новые файлы с цветами Material. Последний добавленный цвет MaterialLime.

---
+++ app/src/main/java/ru/ipixnix/lesson_jpc/ui/theme/material/MaterialLime.kt
@@ -0,0 +1,18 @@
+package ru.ipixnix.lesson_jpc.ui.theme.material
+
+import androidx.compose.ui.graphics.Color
+
+val clrMLime50 = Color(0xFFF9FBE7)
+val clrMLime100 = Color(0xFFF0F4C3)
+val clrMLime200 = Color(0xFFE6EE9C)
+val clrMLime300 = Color(0xFFDCE775)
+val clrMLime400 = Color(0xFFD4E157)
+val clrMLime500 = Color(0xFFCDDC39)
+val clrMLime600 = Color(0xFFC0CA33)
+val clrMLime700 = Color(0xFFAFB42B)
+val clrMLime800 = Color(0xFF9E9D24)
+val clrMLime900 = Color(0xFF827717)
+val clrMLimeA100 = Color(0xFFF4FF81)
+val clrMLimeA200 = Color(0xFFEEFF41)
+val clrMLimeA400 = Color(0xFFC6FF00)
+val clrMLimeA700 = Color(0xFFAEEA00)
