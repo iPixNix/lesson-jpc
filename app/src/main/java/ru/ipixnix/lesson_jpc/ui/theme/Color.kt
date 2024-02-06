9610e4f071e934320ffcff521da7dc752b1b886f
iPixNix <ipixnix@ya.ru>
2024-02-06 13:54:13 +0300

    Update: Сортировка цветов Color.kt
    Update: Сортировка списка цветов в Color.kt

--- app/src/main/java/ru/ipixnix/lesson_jpc/ui/theme/Color.kt
+++ app/src/main/java/ru/ipixnix/lesson_jpc/ui/theme/Color.kt
@@ -4,11 +4,11 @@ package ru.ipixnix.lesson_jpc.ui.theme
 /* Главные цвета */
 val clrDPrimary = clrBlueMiddle // Основной
 val clrDOnPrimary = clrWhite // На Основном
-val clrDInversePrimary = clrYellowMiddle // Инвертированный Основной
 val clrDSecondary = clrBlueDeep // Вторичный
 val clrDOnSecondary = clrWhite // На Вторичном
 val clrDTertiary = clrPurpleMiddle // Третичный
 val clrDOnTertiary = clrWhite // На Третичном
+val clrDInversePrimary = clrYellowMiddle // Инвертированный Основной
 /* Дополнительные цвета */
 val clrDBackground = clrBlackDark // Фон
 val clrDOnBackground = clrWhite // На Фоне
@@ -17,14 +17,6 @@ val clrDOnError = clrWhite // На Ошибке
 val clrDOutline = clrSkyMiddle // Контур
 val clrDOutlineVariant = clrSkyDeep // Контур Вариант
 val clrDScrim = clrGrayMiddle // Сетка
-/* Поверхности */
-val clrDSurface = clrBlueDark // Поверхность
-val clrDOnSurface = clrWhite // На Поверхности
-val clrDInverseSurface = clrYellowDark // Инвертированная Поверхность
-val clrDInverseOnSurface = clrBlack // На Инвертированной Поверхности
-val clrDSurfaceVariant = clrPurpleDark // На Поверхности
-val clrDOnSurfaceVariant = clrWhite // На Поверхности
-val clrDSurfaceTint = clrBlueDark // На Поверхности
 /* Контейнеры */
 val clrDPrimaryContainer = clrBlueMiddle // На Поверхности
 val clrDOnPrimaryContainer = clrWhite // На Поверхности
@@ -34,18 +26,25 @@ val clrDTertiaryContainer = clrPurpleMiddle // На Поверхности
 val clrDOnTertiaryContainer = clrWhite // На Поверхности
 val clrDErrorContainer = clrRedMiddle // Поверхность
 val clrDOnErrorContainer = clrWhite // Поверхность
+/* Поверхности */
+val clrDSurface = clrBlueDark // Поверхность
+val clrDOnSurface = clrWhite // На Поверхности
+val clrDSurfaceVariant = clrPurpleDark // На Поверхности
+val clrDOnSurfaceVariant = clrWhite // На Поверхности
+val clrDInverseSurface = clrYellowDark // Инвертированная Поверхность
+val clrDInverseOnSurface = clrBlack // На Инвертированной Поверхности
+val clrDSurfaceTint = clrBlueDark // На Поверхности
 
 
 /** Светлая тема */
 /* Главные цвета */
 val clrLPrimary = clrBlueMiddle // Основной
 val clrLOnPrimary = clrWhite // На Основном
-val clrLInversePrimary = clrYellowMiddle // Инвертированный Основной
-val clrLInverseOnPrimary = clrBlack // На Инвертированной Поверхности
 val clrLSecondary = clrBlueDeep // Вторичный
 val clrLOnSecondary = clrWhite // На Вторичном
 val clrLTertiary = clrPurpleMiddle // Третичный
 val clrLOnTertiary = clrWhite // На Третичном
+val clrLInversePrimary = clrYellowMiddle // Инвертированный Основной
 /* Дополнительные цвета */
 val clrLBackground = clrWhiteLight // Фон
 val clrLOnBackground = clrBlack // На Фоне
@@ -54,14 +53,6 @@ val clrLOnError = clrWhite // На Ошибке
 val clrLOutline = clrSkyMiddle // Контур
 val clrLOutlineVariant = clrSkyDeep // Контур Вариант
 val clrLScrim = clrGrayMiddle // Сетка
-/* Поверхности */
-val clrLSurface = clrBlueLight // Поверхность
-val clrLOnSurface = clrWhite // На Поверхности
-val clrLInverseSurface = clrYellowLight // Инвертированная Поверхность
-val clrLInverseOnSurface = clrBlack // На Инвертированной Поверхности
-val clrLSurfaceVariant = clrPurpleLight // На Поверхности
-val clrLOnSurfaceVariant = clrWhite // На Поверхности
-val clrLSurfaceTint = clrBlueLight // На Поверхности
 /* Контейнеры */
 val clrLPrimaryContainer = clrBlueMiddle // На Поверхности
 val clrLOnPrimaryContainer = clrWhite // На Поверхности
@@ -71,3 +62,11 @@ val clrLTertiaryContainer = clrPurpleMiddle // На Поверхности
 val clrLOnTertiaryContainer = clrWhite // На Поверхности
 val clrLErrorContainer = clrRedMiddle // Поверхность
 val clrLOnErrorContainer = clrWhite // Поверхность
+/* Поверхности */
+val clrLSurface = clrBlueLight // Поверхность
+val clrLOnSurface = clrWhite // На Поверхности
+val clrLSurfaceVariant = clrPurpleLight // На Поверхности
+val clrLOnSurfaceVariant = clrWhite // На Поверхности
+val clrLInverseSurface = clrYellowLight // Инвертированная Поверхность
+val clrLInverseOnSurface = clrBlack // На Инвертированной Поверхности
+val clrLSurfaceTint = clrBlueLight // На Поверхности
