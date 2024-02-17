09fea11a1be4e14bdfa8dd40559939de65269ac2
iPixNix <ipixnix@ya.ru>
2024-02-17 18:54:02 +0300

    Fix: Build Gradle
    Fix: Build Gradle - удаление нерабочей зависимости.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,6 @@ android {
 
 dependencies {
 
-    // system bars customization
-    implementation ("com.google.accompanist:accompanist-systemuicontroller:1.7")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
